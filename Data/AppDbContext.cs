698a90c85a79820ffaafeb83e5473e3bd1be0029
shafiulsa <shafiulislam.sa@gamil.com>
2025-07-03 16:47:34 +0600

    first commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MiniAccountManagementSystem.Models;
+
+namespace MiniAccountManagementSystem.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+     public DbSet<AccountTypes> AccountTypes { get; set; }
+    }
+}
---
+++ Models/AccountTypes.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniAccountManagementSystem.Models
+{
+    public class AccountTypes
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
